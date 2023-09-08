a23652e77f17dd88113799d976f5a660da61d2ef
Weston Schutt <wtschutt@gmail.com>
2023-09-07 18:12:19 -0600

    Test: Add test for Market poro

---
+++ spec/poros/market_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Market do
+  before(:each) do
+    market_data = {
+      :id=>"322458",
+      :type=>"market",
+      :attributes=>{
+        :name=>"14&U Farmers' Market",
+        :street=>"1400 U Street NW ",
+        :city=>"Washington",
+        :county=>"District of Columbia",
+        :state=>"District of Columbia",
+        :zip=>"20009",
+        :lat=>"38.9169984",
+        :lon=>"-77.0320505",
+        :vendor_count=>1}}
+    
+      @market = Market.new(market_data)
+end
+
+  describe 'initialization' do
+    it 'exists' do
+      expect(@market).to be_a(Market)
+    end
+
+    it 'has the required attributes' do
+      expect(@market.id).to eq("322458")
+      expect(@market.name).to eq("14&U Farmers' Market")
+      expect(@market.city).to eq("Washington")
+      expect(@market.state).to eq("District of Columbia")
+    end
+  end
+
+  describe 'Instance Methods' do
+    
+  end
+end
