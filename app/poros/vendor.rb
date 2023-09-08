e73062308d014680a06c5c389d5731495e481245
Weston Schutt <wtschutt@gmail.com>
2023-09-08 16:45:44 -0600

    Feature: Add vendor poro

---
+++ app/poros/vendor.rb
@@ -0,0 +1,17 @@
+class Vendor
+  attr_reader :id, 
+              :name, 
+              :contact_name, 
+              :contact_phone, 
+              :credit_accepted, 
+              :description
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:attributes][:name]
+    @contact_name = data[:attributes][:contact_name]
+    @contact_phone = data[:attributes][:contact_phone]
+    @credit_accepted = data[:attributes][:credit_accepted]
+    @description = data[:attributes][:description]
+  end
+end
